3f5101206eeaaa03064d6f37721cd800b693d817
Gowthamhm <gowthamhm001@gmail.com>
2021-04-08 08:34:04 +0530

    login check

--- alog.php
+++ alog.php
@@ -6,17 +6,18 @@ include 'error.php';
 if (isset($_POST['login'])) {
   $username = mysqli_real_escape_string($conn,$_POST['username']);
   $password = mysqli_real_escape_string($conn,$_POST['password']);
-  $searchUser = "SELECT * FROM `users` WHERE username =\'".$username."\' and password = \'".$password."\'";
+  $searchUser = "SELECT * FROM `users`";
      // $searchUser = "SELECT * FROM `USERS` WHERE username = '".$username."' and password ='".$password."'";
      echo $searchUser;
      $result = $conn->query($searchUser);
-     echo $result;
+     // echo $result;
      if ($result) {
      if ($result->num_rows > 0) {
        // echo "inside if result";
        while($row = $result->fetch_assoc()) {
     $dbusername = $row['username'];
     $dbpassword = $row['password'];
+    echo $dbusername, $dbpassword;
   }
    if($username==$dbusername && $password == $dbpassword){
      $_SESSION['active_user'] = $dbusername;
