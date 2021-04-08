934cb24b59693b8198f28dbe866ce2f71c3932d6
Gowthamhm <gowthamhm001@gmail.com>
2021-04-08 08:16:25 +0530

    reset password and login

---
+++ alog.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+include 'config.php';
+include 'error.php';
+
+if (isset($_POST['login'])) {
+  $username = mysqli_real_escape_string($conn,$_POST['username']);
+  $password = mysqli_real_escape_string($conn,$_POST['password']);
+     $searchUser = "SELECT * FROM `USERS` WHERE username = '".$username."' and password ='".$password."'";
+     // echo $searchUser;
+     $result = $conn->query($searchUser);
+     // echo $result;
+     // if ($result) {
+     if ($result->num_rows > 0) {
+       // echo "inside if result";
+       while($row = $result->fetch_assoc()) {
+    $dbusername = $row['username'];
+    $dbpassword = $row['password'];
+  }
+   if($username==$dbusername && $password == $dbpassword){
+     $_SESSION['active_user'] = $dbusername;
+     ?><script type="text/javascript" charset="utf-8">
+      alert("Login Successfully ");
+      window.location.replace('home.php');
+      </script>
+      <?php
+   }else{
+      ?>
+      <script type="text/javascript" charset="utf-8">
+       alert("Check The Username And Password ");
+       window.location.replace('home.php');
+       </script>
+       <?php
+   }
+     }else{
+        ?><script type="text/javascript" charset="utf-8">
+  alert("Please Contact Admin to Register ");
+window.location.replace("login.php");
+      </script>
+      <?php
+     }
+}
+else{
+   ?>
+<script type='text/javascript' charset='utf-8'>
+window.location.replace('login.php');
+</script>
+<<?php
+}
+
+ ?>
---
+++ resetpassword.php
@@ -0,0 +1,51 @@
+<?php
+include 'config.php';
+include 'error.php';
+
+if (isset($_POST['reset'])) {
+$username =mysqli_real_escape_string($conn,$_POST['username']);
+$email = mysqli_real_escape_string($conn,$_POST['email']);
+$password =mysqli_real_escape_string($conn,$_POST['newpassword']);
+$confirmPassword =mysqli_real_escape_string($conn,$_POST['confirmpassword']);
+
+if ($password === $confirmPassword) {
+  $searchUser="SELECT * FROM `USERS` where username = '".$username."' and email_id='".$email."';";
+  // echo $searchUser;
+   $result = $conn->query($searchUser);
+   if ($result->num_rows > 0) {
+     while($row = $result->fetch_assoc()) {
+  $dbusername = $row['username'];
+  $dbemail =$row['email_id'];
+}
+// echo $dbusername;
+// echo $dbemail;
+if($dbusername == $username && $dbemail == $email){
+  $update = "UPDATE `USERS` SET `password`='".$password."' WHERE username='".$username."' and email_id='".$email."'";
+  if($conn->query($update) === TRUE){
+    ?><script type="text/javascript" charset="utf-8">
+     alert("Updated Successfully Login using new Password");
+     window.location.replace('login.php');
+     </script>
+     <?php
+  }
+}
+   }
+   ?><script type="text/javascript" charset="utf-8">
+    alert("CHeck the Username or Email Once");
+    window.location.replace('forgot.php');
+    </script>
+    <?php
+}else {
+  ?><script type="text/javascript" charset="utf-8">
+   alert("Password and Confirm Password are not Equal!");
+   window.location.replace('forgot.php');
+   </script>
+   <?php
+}
+}else{
+  ?><script type="text/javascript" charset="utf-8">
+   window.location.replace('forgot.php');
+   </script>
+   <?php
+}
+ ?>
