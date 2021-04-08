2539a209b30c2bdec12880735103189b8965bfc9
Gowthamhm <gowthamhm001@gmail.com>
2021-04-08 21:03:51 +0530

    quick msg update

--- quick_sms.php
+++ quick_sms.php
@@ -9,7 +9,7 @@ if(isset($_POST['sendSMS'])){
 $number = $_POST['number'];
 $text = $_POST['text'];
 
-echo $number,$text;
+// echo $number,$text;
 
 $service_plan_id = "78125b9858494c72894913f48031923d";
 $bearer_token = "63045e8e65ae445b8b65d9f8b7a657cb";
@@ -18,7 +18,7 @@ $send_from = "+447537454577";
 $recipient_phone_numbers = "91".$number; //May be several, separate with a comma `,`.
 $message = $text;
 // "This test message will be sent to {$recipient_phone_numbers} from ";
-echo "$message";
+// echo "$message";
 // Check recipient_phone_numbers for multiple numbers and make it an array.
 if(stristr($recipient_phone_numbers, ',')){
   $recipient_phone_numbers = explode(',', $recipient_phone_numbers);
@@ -49,9 +49,15 @@ $result = curl_exec($ch);
 if(curl_errno($ch)) {
     echo 'Curl error: ' . curl_error($ch);
 } else {
-    echo $result;
+  curl_close($ch);
+  ?><script type="text/javascript" charset="utf-8">
+   alert("Message Sent Successfully");
+   window.location.replace('home.php');
+   </script>
+   <?php
+    // echo $result;
 }
-curl_close($ch);
+
 }else {
   echo "can't able to send SMS";
 }
