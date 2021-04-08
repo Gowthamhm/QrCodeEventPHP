a08a4473f271f38242c7ef550a891f125f10bb98
Gowthamhm <gowthamhm001@gmail.com>
2021-04-08 21:29:32 +0530

    table templeate

--- qrcodeDisplay.php
+++ qrcodeDisplay.php
@@ -4,184 +4,521 @@ include 'error.php';
 include 'session.php';
 include 'nav.php';
 
-if (!empty($_SESSION['folder_name'])) {
-  // echo $_SESSION['folder_name'];
-}else {
-  ?><script type="text/javascript" charset="utf-8">
-   window.location.replace('home.php');
-   </script>
-   <?php
-}
-
-if (!isset ($_GET['page']) ) {
-    $page = 1;
-} else {
-    $page = $_GET['page'];
-}
-// echo $page;
-$results_per_page = 10;
-$page_first_result = ($page-1) * $results_per_page;
-
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
-         <script src="https://cdn.tiny.cloud/1/u5pxigthlaz73eb0hpt89t9lb57oe9f2kgsk4lub57k8r37b/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
-         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-             <meta name="viewport" content="width=device-width,initial-scale=1">
-         <link rel="stylesheet"
-           href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-           crossorigin="anonymous">
-         <link rel="stylesheet" type="text/css" href="assets/css/my-login.css">
-         <link rel="stylesheet" type="text/css" href="assets/css/home.css">
-                  <!-- <script src="assets/css/jquery.min.js" type="text/javascript" charset="utf-8"></script> -->
-         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
-             <title> <?php echo  $_SESSION['folder_name'] ; ?></title>
-  	<meta name="viewport" content="width=device-width,initial-scale=1">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
   </head>
-  <style>
-.pull-xs-left{
-  width: 150px;
-  height: 150px;
-}
-.pagination {
-  display: inline-block;
-
-}
-
-.pagination a {
-  color: black;
-  float: right;
-  padding: 8px 16px;
-  text-decoration: none;
-}
-
-.pagination a.active {
-  background-color: #4CAF50;
-  color: white;
-  border-radius: 5px;
-}
-
-.pagination a:hover:not(.active) {
-  background-color: #ddd;
-  border-radius: 5px;
-}
-/* body {
-  padding:20px;
-  max-width:800px;
-  margin:auto auto;
-  font-family:sans;
-} */
-
-table {
-  width:100%
-} th {
-  background:#666;
-  color:#fff;
-} td {
-  padding:5px;
-}
-
-input {
-  font-size: 18px;
-  padding:2px;
-  border:1px;
-
-}
-  </style>
   <body>
-    <br>
-    <input type="search" class="form-control mr-sm-2" aria-label="Search"  id="search" placeholder="Filter by Text" style="  width: auto;float: right;  height: fit-content;">
-    <br>
-<table class="table">
+    <table id="dtBasicExample" class="table table-striped table-bordered table-sm" cellspacing="0" width="100%">
   <thead>
     <tr>
-      <th>Sl No.</th>
-      <th>Text</th>
-      <th>In QrCode </th>
-      <th>Out QrCode </th>
-      <th>Status</th>
+      <th class="th-sm">Name
+
+      </th>
+      <th class="th-sm">Position
+
+      </th>
+      <th class="th-sm">Office
+
+      </th>
+      <th class="th-sm">Age
+
+      </th>
+      <th class="th-sm">Start date
+
+      </th>
+      <th class="th-sm">Salary
+
+      </th>
     </tr>
   </thead>
-
-  <?php
-$select = "SELECT * from qrcode where folder_name = '".$_SESSION['folder_name']."'";
-$result = $conn->query($select);
-$number_of_result = mysqli_num_rows($result);
-//determine the total number of pages available
-$number_of_page = ceil ($number_of_result / $results_per_page);
-$query = "SELECT * from qrcode where folder_name = '".$_SESSION['folder_name']."' LIMIT " . $page_first_result . ',' . $results_per_page;
-// echo $query;
-   $result = $conn->query($query);
-if ($result->num_rows > 0){
-  $count = 1;
-     while($row = $result->fetch_assoc()){
-       ?> <tbody>
-         <tr>
-           <td> <?php echo $count; ?></td><td id="title">
-           <?php echo $row['text'];?></td><td> <img src='<?php echo($row['path'].'/'.$row['infilename']);  ?>' class="img-fluid pull-xs-left" alt="...">
-           </td><td> <img src='<?php echo($row['path'].'/'.$row['outfilename']); ?>' class="img-fluid pull-xs-left" alt="...">
-           </td><td>
-           <?php if($row['status'] == 0){
-              echo "Not Shared Yet";
-           }else if($row['status'] == 1){
-             echo "Shared Already";
-           }else{
-             echo "Already Scanned";
-           }
-           $count++;
-     }
-}
-  ?>
-</td>
+  <tbody>
+    <tr>
+      <td>Tiger Nixon</td>
+      <td>System Architect</td>
+      <td>Edinburgh</td>
+      <td>61</td>
+      <td>2011/04/25</td>
+      <td>$320,800</td>
+    </tr>
+    <tr>
+      <td>Garrett Winters</td>
+      <td>Accountant</td>
+      <td>Tokyo</td>
+      <td>63</td>
+      <td>2011/07/25</td>
+      <td>$170,750</td>
+    </tr>
+    <tr>
+      <td>Ashton Cox</td>
+      <td>Junior Technical Author</td>
+      <td>San Francisco</td>
+      <td>66</td>
+      <td>2009/01/12</td>
+      <td>$86,000</td>
+    </tr>
+    <tr>
+      <td>Cedric Kelly</td>
+      <td>Senior Javascript Developer</td>
+      <td>Edinburgh</td>
+      <td>22</td>
+      <td>2012/03/29</td>
+      <td>$433,060</td>
+    </tr>
+    <tr>
+      <td>Airi Satou</td>
+      <td>Accountant</td>
+      <td>Tokyo</td>
+      <td>33</td>
+      <td>2008/11/28</td>
+      <td>$162,700</td>
+    </tr>
+    <tr>
+      <td>Brielle Williamson</td>
+      <td>Integration Specialist</td>
+      <td>New York</td>
+      <td>61</td>
+      <td>2012/12/02</td>
+      <td>$372,000</td>
+    </tr>
+    <tr>
+      <td>Herrod Chandler</td>
+      <td>Sales Assistant</td>
+      <td>San Francisco</td>
+      <td>59</td>
+      <td>2012/08/06</td>
+      <td>$137,500</td>
+    </tr>
+    <tr>
+      <td>Rhona Davidson</td>
+      <td>Integration Specialist</td>
+      <td>Tokyo</td>
+      <td>55</td>
+      <td>2010/10/14</td>
+      <td>$327,900</td>
+    </tr>
+    <tr>
+      <td>Colleen Hurst</td>
+      <td>Javascript Developer</td>
+      <td>San Francisco</td>
+      <td>39</td>
+      <td>2009/09/15</td>
+      <td>$205,500</td>
+    </tr>
+    <tr>
+      <td>Sonya Frost</td>
+      <td>Software Engineer</td>
+      <td>Edinburgh</td>
+      <td>23</td>
+      <td>2008/12/13</td>
+      <td>$103,600</td>
+    </tr>
+    <tr>
+      <td>Jena Gaines</td>
+      <td>Office Manager</td>
+      <td>London</td>
+      <td>30</td>
+      <td>2008/12/19</td>
+      <td>$90,560</td>
+    </tr>
+    <tr>
+      <td>Quinn Flynn</td>
+      <td>Support Lead</td>
+      <td>Edinburgh</td>
+      <td>22</td>
+      <td>2013/03/03</td>
+      <td>$342,000</td>
+    </tr>
+    <tr>
+      <td>Charde Marshall</td>
+      <td>Regional Director</td>
+      <td>San Francisco</td>
+      <td>36</td>
+      <td>2008/10/16</td>
+      <td>$470,600</td>
+    </tr>
+    <tr>
+      <td>Haley Kennedy</td>
+      <td>Senior Marketing Designer</td>
+      <td>London</td>
+      <td>43</td>
+      <td>2012/12/18</td>
+      <td>$313,500</td>
+    </tr>
+    <tr>
+      <td>Tatyana Fitzpatrick</td>
+      <td>Regional Director</td>
+      <td>London</td>
+      <td>19</td>
+      <td>2010/03/17</td>
+      <td>$385,750</td>
+    </tr>
+    <tr>
+      <td>Michael Silva</td>
+      <td>Marketing Designer</td>
+      <td>London</td>
+      <td>66</td>
+      <td>2012/11/27</td>
+      <td>$198,500</td>
+    </tr>
+    <tr>
+      <td>Paul Byrd</td>
+      <td>Chief Financial Officer (CFO)</td>
+      <td>New York</td>
+      <td>64</td>
+      <td>2010/06/09</td>
+      <td>$725,000</td>
+    </tr>
+    <tr>
+      <td>Gloria Little</td>
+      <td>Systems Administrator</td>
+      <td>New York</td>
+      <td>59</td>
+      <td>2009/04/10</td>
+      <td>$237,500</td>
+    </tr>
+    <tr>
+      <td>Bradley Greer</td>
+      <td>Software Engineer</td>
+      <td>London</td>
+      <td>41</td>
+      <td>2012/10/13</td>
+      <td>$132,000</td>
+    </tr>
+    <tr>
+      <td>Dai Rios</td>
+      <td>Personnel Lead</td>
+      <td>Edinburgh</td>
+      <td>35</td>
+      <td>2012/09/26</td>
+      <td>$217,500</td>
+    </tr>
+    <tr>
+      <td>Jenette Caldwell</td>
+      <td>Development Lead</td>
+      <td>New York</td>
+      <td>30</td>
+      <td>2011/09/03</td>
+      <td>$345,000</td>
+    </tr>
+    <tr>
+      <td>Yuri Berry</td>
+      <td>Chief Marketing Officer (CMO)</td>
+      <td>New York</td>
+      <td>40</td>
+      <td>2009/06/25</td>
+      <td>$675,000</td>
+    </tr>
+    <tr>
+      <td>Caesar Vance</td>
+      <td>Pre-Sales Support</td>
+      <td>New York</td>
+      <td>21</td>
+      <td>2011/12/12</td>
+      <td>$106,450</td>
+    </tr>
+    <tr>
+      <td>Doris Wilder</td>
+      <td>Sales Assistant</td>
+      <td>Sidney</td>
+      <td>23</td>
+      <td>2010/09/20</td>
+      <td>$85,600</td>
+    </tr>
+    <tr>
+      <td>Angelica Ramos</td>
+      <td>Chief Executive Officer (CEO)</td>
+      <td>London</td>
+      <td>47</td>
+      <td>2009/10/09</td>
+      <td>$1,200,000</td>
+    </tr>
+    <tr>
+      <td>Gavin Joyce</td>
+      <td>Developer</td>
+      <td>Edinburgh</td>
+      <td>42</td>
+      <td>2010/12/22</td>
+      <td>$92,575</td>
+    </tr>
+    <tr>
+      <td>Jennifer Chang</td>
+      <td>Regional Director</td>
+      <td>Singapore</td>
+      <td>28</td>
+      <td>2010/11/14</td>
+      <td>$357,650</td>
+    </tr>
+    <tr>
+      <td>Brenden Wagner</td>
+      <td>Software Engineer</td>
+      <td>San Francisco</td>
+      <td>28</td>
+      <td>2011/06/07</td>
+      <td>$206,850</td>
+    </tr>
+    <tr>
+      <td>Fiona Green</td>
+      <td>Chief Operating Officer (COO)</td>
+      <td>San Francisco</td>
+      <td>48</td>
+      <td>2010/03/11</td>
+      <td>$850,000</td>
+    </tr>
+    <tr>
+      <td>Shou Itou</td>
+      <td>Regional Marketing</td>
+      <td>Tokyo</td>
+      <td>20</td>
+      <td>2011/08/14</td>
+      <td>$163,000</td>
+    </tr>
+    <tr>
+      <td>Michelle House</td>
+      <td>Integration Specialist</td>
+      <td>Sidney</td>
+      <td>37</td>
+      <td>2011/06/02</td>
+      <td>$95,400</td>
+    </tr>
+    <tr>
+      <td>Suki Burks</td>
+      <td>Developer</td>
+      <td>London</td>
+      <td>53</td>
+      <td>2009/10/22</td>
+      <td>$114,500</td>
+    </tr>
+    <tr>
+      <td>Prescott Bartlett</td>
+      <td>Technical Author</td>
+      <td>London</td>
+      <td>27</td>
+      <td>2011/05/07</td>
+      <td>$145,000</td>
+    </tr>
+    <tr>
+      <td>Gavin Cortez</td>
+      <td>Team Leader</td>
+      <td>San Francisco</td>
+      <td>22</td>
+      <td>2008/10/26</td>
+      <td>$235,500</td>
+    </tr>
+    <tr>
+      <td>Martena Mccray</td>
+      <td>Post-Sales support</td>
+      <td>Edinburgh</td>
+      <td>46</td>
+      <td>2011/03/09</td>
+      <td>$324,050</td>
+    </tr>
+    <tr>
+      <td>Unity Butler</td>
+      <td>Marketing Designer</td>
+      <td>San Francisco</td>
+      <td>47</td>
+      <td>2009/12/09</td>
+      <td>$85,675</td>
+    </tr>
+    <tr>
+      <td>Howard Hatfield</td>
+      <td>Office Manager</td>
+      <td>San Francisco</td>
+      <td>51</td>
+      <td>2008/12/16</td>
+      <td>$164,500</td>
+    </tr>
+    <tr>
+      <td>Hope Fuentes</td>
+      <td>Secretary</td>
+      <td>San Francisco</td>
+      <td>41</td>
+      <td>2010/02/12</td>
+      <td>$109,850</td>
+    </tr>
+    <tr>
+      <td>Vivian Harrell</td>
+      <td>Financial Controller</td>
+      <td>San Francisco</td>
+      <td>62</td>
+      <td>2009/02/14</td>
+      <td>$452,500</td>
+    </tr>
+    <tr>
+      <td>Timothy Mooney</td>
+      <td>Office Manager</td>
+      <td>London</td>
+      <td>37</td>
+      <td>2008/12/11</td>
+      <td>$136,200</td>
+    </tr>
+    <tr>
+      <td>Jackson Bradshaw</td>
+      <td>Director</td>
+      <td>New York</td>
+      <td>65</td>
+      <td>2008/09/26</td>
+      <td>$645,750</td>
+    </tr>
+    <tr>
+      <td>Olivia Liang</td>
+      <td>Support Engineer</td>
+      <td>Singapore</td>
+      <td>64</td>
+      <td>2011/02/03</td>
+      <td>$234,500</td>
+    </tr>
+    <tr>
+      <td>Bruno Nash</td>
+      <td>Software Engineer</td>
+      <td>London</td>
+      <td>38</td>
+      <td>2011/05/03</td>
+      <td>$163,500</td>
+    </tr>
+    <tr>
+      <td>Sakura Yamamoto</td>
+      <td>Support Engineer</td>
+      <td>Tokyo</td>
+      <td>37</td>
+      <td>2009/08/19</td>
+      <td>$139,575</td>
+    </tr>
+    <tr>
+      <td>Thor Walton</td>
+      <td>Developer</td>
+      <td>New York</td>
+      <td>61</td>
+      <td>2013/08/11</td>
+      <td>$98,540</td>
+    </tr>
+    <tr>
+      <td>Finn Camacho</td>
+      <td>Support Engineer</td>
+      <td>San Francisco</td>
+      <td>47</td>
+      <td>2009/07/07</td>
+      <td>$87,500</td>
+    </tr>
+    <tr>
+      <td>Serge Baldwin</td>
+      <td>Data Coordinator</td>
+      <td>Singapore</td>
+      <td>64</td>
+      <td>2012/04/09</td>
+      <td>$138,575</td>
+    </tr>
+    <tr>
+      <td>Zenaida Frank</td>
+      <td>Software Engineer</td>
+      <td>New York</td>
+      <td>63</td>
+      <td>2010/01/04</td>
+      <td>$125,250</td>
+    </tr>
+    <tr>
+      <td>Zorita Serrano</td>
+      <td>Software Engineer</td>
+      <td>San Francisco</td>
+      <td>56</td>
+      <td>2012/06/01</td>
+      <td>$115,000</td>
+    </tr>
+    <tr>
+      <td>Jennifer Acosta</td>
+      <td>Junior Javascript Developer</td>
+      <td>Edinburgh</td>
+      <td>43</td>
+      <td>2013/02/01</td>
+      <td>$75,650</td>
+    </tr>
+    <tr>
+      <td>Cara Stevens</td>
+      <td>Sales Assistant</td>
+      <td>New York</td>
+      <td>46</td>
+      <td>2011/12/06</td>
+      <td>$145,600</td>
+    </tr>
+    <tr>
+      <td>Hermione Butler</td>
+      <td>Regional Director</td>
+      <td>London</td>
+      <td>47</td>
+      <td>2011/03/21</td>
+      <td>$356,250</td>
+    </tr>
+    <tr>
+      <td>Lael Greer</td>
+      <td>Systems Administrator</td>
+      <td>London</td>
+      <td>21</td>
+      <td>2009/02/27</td>
+      <td>$103,500</td>
+    </tr>
+    <tr>
+      <td>Jonas Alexander</td>
+      <td>Developer</td>
+      <td>San Francisco</td>
+      <td>30</td>
+      <td>2010/07/14</td>
+      <td>$86,500</td>
+    </tr>
+    <tr>
+      <td>Shad Decker</td>
+      <td>Regional Director</td>
+      <td>Edinburgh</td>
+      <td>51</td>
+      <td>2008/11/13</td>
+      <td>$183,000</td>
+    </tr>
+    <tr>
+      <td>Michael Bruce</td>
+      <td>Javascript Developer</td>
+      <td>Singapore</td>
+      <td>29</td>
+      <td>2011/06/27</td>
+      <td>$183,000</td>
+    </tr>
+    <tr>
+      <td>Donna Snider</td>
+      <td>Customer Support</td>
+      <td>New York</td>
+      <td>27</td>
+      <td>2011/01/25</td>
+      <td>$112,000</td>
     </tr>
   </tbody>
+  <tfoot>
+    <tr>
+      <th>Name
+      </th>
+      <th>Position
+      </th>
+      <th>Office
+      </th>
+      <th>Age
+      </th>
+      <th>Start date
+      </th>
+      <th>Salary
+      </th>
+    </tr>
+  </tfoot>
 </table>
-<div class="pagination" id="pagenation">
-<?php
-if($page > 1){
-    echo '<a class="btn btn-primary" href = "qrcodeDisplay.php?page=' . --$page . '">&laquo; </a>';
-  }else{
-  }
-for($page = 1; $page<= $number_of_page; $page++) {
-    echo '<a class="btn btn-primary" id="'.$page.'" href = "qrcodeDisplay.php?page=' . $page . '">' . $page . ' </a>';
-  }
-  if($page > 2){
-    echo '<a class="btn btn-primary" href = "qrcodeDisplay.php?page=' . $page++ . '">&raquo; </a>';
-  }else{
-
-    }
- ?>
-   <!-- <a href = "qrcodeDisplay.php?page='".$page++."'">&raquo;</a> -->
-</div>
   </body>
-  <script type="text/javascript">
-  // Quick Table Search
-$('#search').keyup(function() {
-var regex = new RegExp($('#search').val(), "i");
-var rows = $('table tr:gt(0)');
-rows.each(function (index) {
-  title = $(this).children("#title").html()
-  if (title.search(regex) != -1) {
-    $(this).show();
-  } else {
-    $(this).hide();
-  }
-});
-});
-
-var header = document.getElementById("pagenation");
-var btns = header.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
-  });
-}
-  </script>
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 </html>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js" integrity="sha384-LtrjvnR4Twt/qOuYxE721u19sVFLVSA4hf/rRt6PrZTmiPltdZcI7q7PXQBYTKyf" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script type="text/javascript">
+$(document).ready(function () {
+$('#dtBasicExample').DataTable();
+$('.dataTables_length').addClass('bs-select');
+});
+</script>
