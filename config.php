19d763cb75321628d5fde6739847c35d2549f7e8
Gowthamhm <gowthamhm001@gmail.com>
2021-04-08 07:43:11 +0530

    db config added

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+ $servername = "localhost";
+ $username = "u514971509_root";
+ $password = "Gowthamhm001@";
+ $dbname="u514971509_qrcode";
+
+//$servername ="localhost";
+//$username ="root";
+//$password ="";
+//$dbname ="qrcode";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}else{
+// echo "Connected successfully";
+}
+?>
