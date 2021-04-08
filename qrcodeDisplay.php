a2f8768c4a15c5823bc92c0a277d70c8bdbaa6ac
Gowthamhm <gowthamhm001@gmail.com>
2021-04-08 22:03:36 +0530

    table

--- qrcodeDisplay.php
+++ qrcodeDisplay.php
@@ -14,228 +14,59 @@ include 'session.php';
     <!-- Google Fonts Roboto -->
     <link    rel="stylesheet"      href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"/>
         <link rel="stylesheet" href="css/mdb.min.css" />
+        <link rel="stylesheet" href="css/table.css" />
   </head>
   <body>
-    <h2 class='mb-3'>Basic example</h2>
-    <table id="dtBasicExample" class="table" width="100%">
-      <thead>
+    <h1><span class="blue">&lt;</span>Table<span class="blue">&gt;</span> <span class="yellow">Responsive</pan></h1>
+    <h2>Created with love by <a href="https://github.com/pablorgarcia" target="_blank">Pablo García</a></h2>
+
+    <table class="container">
+    	<thead>
+    		<tr>
+    			<th><h1>Sites</h1></th>
+    			<th><h1>Views</h1></th>
+    			<th><h1>Clicks</h1></th>
+    			<th><h1>Average</h1></th>
+    		</tr>
+    	</thead>
+    	<tbody>
+    		<tr>
+    			<td>Google</td>
+    			<td>9518</td>
+    			<td>6369</td>
+    			<td>01:32:50</td>
+    		</tr>
+    		<tr>
+    			<td>Twitter</td>
+    			<td>7326</td>
+    			<td>10437</td>
+    			<td>00:51:22</td>
+    		</tr>
+    		<tr>
+    			<td>Amazon</td>
+    			<td>4162</td>
+    			<td>5327</td>
+    			<td>00:24:34</td>
+    		</tr>
         <tr>
-          <th class="th-sm">Name
-          </th>
-          <th class="th-sm">Position
-          </th>
-          <th class="th-sm">Office
-          </th>
-          <th class="th-sm">Age
-          </th>
-          <th class="th-sm">Start date
-          </th>
-          <th class="th-sm">Salary
-          </th>
-        </tr>
-      </thead>
-      <tbody>
+    			<td>LinkedIn</td>
+    			<td>3654</td>
+    			<td>2961</td>
+    			<td>00:12:10</td>
+    		</tr>
         <tr>
-          <td>Tiger Nixon</td>
-          <td>System Architect</td>
-          <td>Edinburgh</td>
-          <td>61</td>
-          <td>2011/04/25</td>
-          <td>$320,800</td>
-        </tr>
+    			<td>CodePen</td>
+    			<td>2002</td>
+    			<td>4135</td>
+    			<td>00:46:19</td>
+    		</tr>
         <tr>
-          <td>Garrett Winters</td>
-          <td>Accountant</td>
-          <td>Tokyo</td>
-          <td>63</td>
-          <td>2011/07/25</td>
-          <td>$170,750</td>
-        </tr>
-        <tr>
-          <td>Ashton Cox</td>
-          <td>Junior Technical Author</td>
-          <td>San Francisco</td>
-          <td>66</td>
-          <td>2009/01/12</td>
-          <td>$86,000</td>
-        </tr>
-        <tr>
-          <td>Cedric Kelly</td>
-          <td>Senior Javascript Developer</td>
-          <td>Edinburgh</td>
-          <td>22</td>
-          <td>2012/03/29</td>
-          <td>$433,060</td>
-        </tr>
-        <tr>
-          <td>Airi Satou</td>
-          <td>Accountant</td>
-          <td>Tokyo</td>
-          <td>33</td>
-          <td>2008/11/28</td>
-          <td>$162,700</td>
-        </tr>
-        <tr>
-          <td>Brielle Williamson</td>
-          <td>Integration Specialist</td>
-          <td>New York</td>
-          <td>61</td>
-          <td>2012/12/02</td>
-          <td>$372,000</td>
-        </tr>
-        <tr>
-          <td>Herrod Chandler</td>
-          <td>Sales Assistant</td>
-          <td>San Francisco</td>
-          <td>59</td>
-          <td>2012/08/06</td>
-          <td>$137,500</td>
-        </tr>
-        <tr>
-          <td>Rhona Davidson</td>
-          <td>Integration Specialist</td>
-          <td>Tokyo</td>
-          <td>55</td>
-          <td>2010/10/14</td>
-          <td>$327,900</td>
-        </tr>
-        <tr>
-          <td>Colleen Hurst</td>
-          <td>Javascript Developer</td>
-          <td>San Francisco</td>
-          <td>39</td>
-          <td>2009/09/15</td>
-          <td>$205,500</td>
-        </tr>
-        <tr>
-          <td>Sonya Frost</td>
-          <td>Software Engineer</td>
-          <td>Edinburgh</td>
-          <td>23</td>
-          <td>2008/12/13</td>
-          <td>$103,600</td>
-        </tr>
-        <tr>
-          <td>Jena Gaines</td>
-          <td>Office Manager</td>
-          <td>London</td>
-          <td>30</td>
-          <td>2008/12/19</td>
-          <td>$90,560</td>
-        </tr>
-        <tr>
-          <td>Quinn Flynn</td>
-          <td>Support Lead</td>
-          <td>Edinburgh</td>
-          <td>22</td>
-          <td>2013/03/03</td>
-          <td>$342,000</td>
-        </tr>
-        <tr>
-          <td>Charde Marshall</td>
-          <td>Regional Director</td>
-          <td>San Francisco</td>
-          <td>36</td>
-          <td>2008/10/16</td>
-          <td>$470,600</td>
-        </tr>
-        <tr>
-          <td>Haley Kennedy</td>
-          <td>Senior Marketing Designer</td>
-          <td>London</td>
-          <td>43</td>
-          <td>2012/12/18</td>
-          <td>$313,500</td>
-        </tr>
-        <tr>
-          <td>Tatyana Fitzpatrick</td>
-          <td>Regional Director</td>
-          <td>London</td>
-          <td>19</td>
-          <td>2010/03/17</td>
-          <td>$385,750</td>
-        </tr>
-        <tr>
-          <td>Michael Silva</td>
-          <td>Marketing Designer</td>
-          <td>London</td>
-          <td>66</td>
-          <td>2012/11/27</td>
-          <td>$198,500</td>
-        </tr>
-        <tr>
-          <td>Paul Byrd</td>
-          <td>Chief Financial Officer (CFO)</td>
-          <td>New York</td>
-          <td>64</td>
-          <td>2010/06/09</td>
-          <td>$725,000</td>
-        </tr>
-        <tr>
-          <td>Gloria Little</td>
-          <td>Systems Administrator</td>
-          <td>New York</td>
-          <td>59</td>
-          <td>2009/04/10</td>
-          <td>$237,500</td>
-        </tr>
-        <tr>
-          <td>Bradley Greer</td>
-          <td>Software Engineer</td>
-          <td>London</td>
-          <td>41</td>
-          <td>2012/10/13</td>
-          <td>$132,000</td>
-        </tr>
-        <tr>
-          <td>Dai Rios</td>
-          <td>Personnel Lead</td>
-          <td>Edinburgh</td>
-          <td>35</td>
-          <td>2012/09/26</td>
-          <td>$217,500</td>
-        </tr>
-        <tr>
-          <td>Jenette Caldwell</td>
-          <td>Development Lead</td>
-          <td>New York</td>
-          <td>30</td>
-          <td>2011/09/03</td>
-          <td>$345,000</td>
-        </tr>
-        <tr>
-          <td>Yuri Berry</td>
-          <td>Chief Marketing Officer (CMO)</td>
-          <td>New York</td>
-          <td>40</td>
-          <td>2009/06/25</td>
-          <td>$675,000</td>
-        </tr>
-        <tr>
-          <td>Caesar Vance</td>
-          <td>Pre-Sales Support</td>
-          <td>New York</td>
-          <td>21</td>
-          <td>2011/12/12</td>
-          <td>$106,450</td>
-        </tr>
-      </tbody>
-      <tfoot>
-        <tr>
-          <th>Name
-          </th>
-          <th>Position
-          </th>
-          <th>Office
-          </th>
-          <th>Age
-          </th>
-          <th>Start date
-          </th>
-          <th>Salary
-          </th>
-        </tr>
-      </tfoot>
+    			<td>GitHub</td>
+    			<td>4623</td>
+    			<td>3486</td>
+    			<td>00:31:52</td>
+    		</tr>
+    	</tbody>
     </table>
   </body>
 </html>
@@ -243,7 +74,15 @@ include 'session.php';
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js" integrity="sha384-LtrjvnR4Twt/qOuYxE721u19sVFLVSA4hf/rRt6PrZTmiPltdZcI7q7PXQBYTKyf" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="js/mdb.min.js"></script>
+<!-- DataTables CSS -->
+<link href="css/addons/datatables2.min.css" rel="stylesheet">
+<!-- DataTables JS -->
+<script src="js/addons/datatables2.min.js" type="text/javascript"></script>
+
+<!-- DataTables Select CSS -->
+<link href="css/addons/datatables-select2.min.css" rel="stylesheet">
+<!-- DataTables Select JS -->
+<script src="js/addons/datatables-select2.min.js" type="text/javascript"></script>
 <script type="text/javascript">
 $(document).ready(function () {
   $('#dtBasicExample').DataTable();
