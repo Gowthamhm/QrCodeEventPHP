b64dcfd2a9e5af8a101b84180cdfb41ff47f6c84
Gowthamhm <gowthamhm001@gmail.com>
2021-04-09 13:33:21 +0530

    sms

--- quick_sms.php
+++ quick_sms.php
@@ -9,16 +9,16 @@ if(isset($_POST['sendSMS'])){
 $number = $_POST['number'];
 $text = $_POST['text'];
 
-// echo $number,$text;
+echo $number,$text;
 
 $service_plan_id = "78125b9858494c72894913f48031923d";
 $bearer_token = "63045e8e65ae445b8b65d9f8b7a657cb";
 
 $send_from = "+447537454577";
 $recipient_phone_numbers = "91".$number; //May be several, separate with a comma `,`.
-// $message = $text;
+$message = $text;
 // "This test message will be sent to {$recipient_phone_numbers} from ";
-// echo "$message";
+echo "$message";
 // Check recipient_phone_numbers for multiple numbers and make it an array.
 if(stristr($recipient_phone_numbers, ',')){
   $recipient_phone_numbers = explode(',', $recipient_phone_numbers);
@@ -34,7 +34,7 @@ $content = [
 ];
 
 $data = json_encode($content);
-
+echo $data;
 $ch = curl_init("https://us.sms.api.sinch.com/xms/v1/{$service_plan_id}/batches");
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
 curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BEARER);
