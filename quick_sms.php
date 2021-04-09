affeac28e87b47e346fe641cafda41361ac021ec
Gowthamhm <gowthamhm001@gmail.com>
2021-04-09 13:52:12 +0530

    sms

--- quick_sms.php
+++ quick_sms.php
@@ -9,56 +9,84 @@ if(isset($_POST['sendSMS'])){
 $number = $_POST['number'];
 $text = $_POST['text'];
 
-echo $number,$text;
-
-$service_plan_id = "78125b9858494c72894913f48031923d";
-$bearer_token = "63045e8e65ae445b8b65d9f8b7a657cb";
-
-$send_from = "+447537454577";
-$recipient_phone_numbers = "91".$number; //May be several, separate with a comma `,`.
-$message = $text;
-// "This test message will be sent to {$recipient_phone_numbers} from ";
-// echo "$message";
+// echo $number,$text;
+$recipient_phone_numbers = "91".$number;
 // Check recipient_phone_numbers for multiple numbers and make it an array.
 if(stristr($recipient_phone_numbers, ',')){
   $recipient_phone_numbers = explode(',', $recipient_phone_numbers);
 }else{
   $recipient_phone_numbers = [$recipient_phone_numbers];
 }
+// Account details
+$apiKey = urlencode(ZjY0YTM3ZjE4ZGU2MzdkMDFmYTAwMzQ5ZjU4YmMwMjQ=);
+// Message details
+$numbers = $recipient_phone_numbers;
+$sender = urlencode(‘TXTLCL’);
+$message = rawurlencode($text);
 
-// Set necessary fields to be JSON encoded
-$content = [
-  'to' => array_values($recipient_phone_numbers),
-  'from' => $send_from,
-  'body' => $_POST['text']
-];
+$numbers = implode(‘,’, $numbers);
 
-$data = json_encode($content);
-echo $data;
-$ch = curl_init("https://us.sms.api.sinch.com/xms/v1/{$service_plan_id}/batches");
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BEARER);
-curl_setopt($ch, CURLOPT_XOAUTH2_BEARER, $bearer_token);
+// Prepare data for POST request
+$data = array(‘apikey’ => $apiKey, ‘numbers’ => $numbers, “sender” => $sender, “message” => $message);
+// Send the POST request with cURL
+$ch = curl_init(‘https://api.textlocal.in/send/’);
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-
-$result = curl_exec($ch);
-
-if(curl_errno($ch)) {
-    echo 'Curl error: ' . curl_error($ch);
-} else {
-  curl_close($ch);
-  ?>
-  <!-- <script type="text/javascript" charset="utf-8">
-   alert("Message Sent Successfully");
-   window.location.replace('home.php');
-   </script> -->
-   <?php
-    echo $result;
-}
-
+$response = curl_exec($ch);
+curl_close($ch);
+// Process your response here
+echo $response;
+//
+//
+// $service_plan_id = "78125b9858494c72894913f48031923d";
+// $bearer_token = "63045e8e65ae445b8b65d9f8b7a657cb";
+//
+// $send_from = "+447537454577";
+// $recipient_phone_numbers = "91".$number; //May be several, separate with a comma `,`.
+// $message = $text;
+// // "This test message will be sent to {$recipient_phone_numbers} from ";
+// // echo "$message";
+// // Check recipient_phone_numbers for multiple numbers and make it an array.
+// if(stristr($recipient_phone_numbers, ',')){
+//   $recipient_phone_numbers = explode(',', $recipient_phone_numbers);
+// }else{
+//   $recipient_phone_numbers = [$recipient_phone_numbers];
+// }
+//
+// // Set necessary fields to be JSON encoded
+// $content = [
+//   'to' => array_values($recipient_phone_numbers),
+//   'from' => $send_from,
+//   'body' => $_POST['text']
+// ];
+//
+// $data = json_encode($content);
+// echo $data;
+// $ch = curl_init("https://us.sms.api.sinch.com/xms/v1/{$service_plan_id}/batches");
+// curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+// curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BEARER);
+// curl_setopt($ch, CURLOPT_XOAUTH2_BEARER, $bearer_token);
+// curl_setopt($ch, CURLOPT_POST, true);
+// curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+//
+// $result = curl_exec($ch);
+//
+// if(curl_errno($ch)) {
+//     echo 'Curl error: ' . curl_error($ch);
+// } else {
+//   curl_close($ch);
+?>
+   <!-- <script type="text/javascript" charset="utf-8">
+    alert("Message Sent Successfully");
+    window.location.replace('home.php');
+      </script> -->
+    <?php
+//     echo $result;
+// }
+//
 }else {
   echo "can't able to send SMS";
 }
