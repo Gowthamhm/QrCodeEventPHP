38dd402694c3d9433a5e9c578ff689ea817f1108
Gowthamhm <gowthamhm001@gmail.com>
2021-04-11 19:23:47 +0530

    hidd

--- qrcodeDisplay.php
+++ qrcodeDisplay.php
@@ -237,7 +237,7 @@ function uncheckAll() {
              var hidden = document.getElementById('hidden');
              for (var checkbox of markedCheckbox) {
                if (checkbox.checked)
-                 hidden.append(checkbox.value + ' ');
+                 hidden..innerHTML=checkbox.value + ' ';
              }
            }
            </script>
