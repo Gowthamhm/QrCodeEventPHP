da7d65b53b0c938d7c50e78842f53b9c23ccfcb5
Gowthamhm <gowthamhm001@gmail.com>
2021-04-11 18:28:05 +0530

    config

--- alog.php
+++ alog.php
@@ -54,7 +54,7 @@ else{
 <script type='text/javascript' charset='utf-8'>
 window.location.replace('login.php');
 </script>
-<<?php
+<?php
 }
 
  ?>
--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
- // $servername = "localhost";
- // $username = "u514971509_root";
- // $password = "Gowthamhm001@";
- // $dbname="u514971509_qrcode";
+ $servername = "localhost";
+ $username = "u514971509_root";
+ $password = "Gowthamhm001@";
+ $dbname="u514971509_qrcode";
 
-$servername ="localhost";
-$username ="root";
-$password ="";
-$dbname ="qrcode";
+// $servername ="localhost";
+// $username ="root";
+// $password ="";
+// $dbname ="qrcode";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password,$dbname);
@@ -15,6 +15,12 @@ $conn = new mysqli($servername, $username, $password,$dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
+  ?>
+<script type='text/javascript' charset='utf-8'>
+alert("Please Contact Admin with Error Message");
+window.location.replace('login.php');
+</script>
+<?php
 }else{
 // echo "Connected successfully";
 }
